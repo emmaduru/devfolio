9521d5c0345ae41baf87775e7b1eef1239ccd427
emmanuella <emmavduru@gmail.com>
2024-02-13 10:08:56 +0000

    add staticfiles and edit project migrations

--- devfolio_project/settings.py
+++ devfolio_project/settings.py
@@ -145,7 +145,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = BASE_DIR / 'productionfiles'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.getenv("CLOUDINARY_CLOUD_NAME"),
